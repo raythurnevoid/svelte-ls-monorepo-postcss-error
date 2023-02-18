0a1de74708bcfccb085e6be8f25866684a4305ed
Ray Thurne Void <ray.thurne.void@gmail.com>
2023-02-18 23:39:28 +0000

    initial commit

---
+++ test-workspace/src/lib/components/Button.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	let buttonEl: HTMLButtonElement;
+</script>
+
+<div class="Button mdc-touch-target-wrapper">
+	<button bind:this={buttonEl} class="mdc-button mdc-button--touch mdc-button--raised">
+		<span class="mdc-button__ripple" />
+		<span class="mdc-button__touch" />
+		<span class="mdc-button__label">My Accessible Button</span>
+	</button>
+</div>
+
+<style lang="postcss">
+	.Button {
+		.mdc-button {
+			color: red;
+		}
+	}
+</style>
